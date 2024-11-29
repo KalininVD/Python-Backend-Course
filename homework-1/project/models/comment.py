16a3771cbce1a4152c935408828d05441089c7ff
Vlad_K <kalinin.vd@yandex.ru>
2024-11-29 15:44:02 +0300

    Added types annotations and docstrings for all implemented methods

--- homework-1/project/models/comment.py
+++ homework-1/project/models/comment.py
@@ -1,23 +1,51 @@
 from datetime import datetime
 
+from uuid import UUID
 
 class Comment:
-    def __init__(self, author_id, text):
+    """Class for modeling Comment entity
+    
+    Includes comment author ID, comment text, creation and update dates and like counter."""
+
+    def __init__(self, author_id: int | UUID, text: str) -> None:
+        """Constructor for Comment class
+
+        Initializes comment author ID and text fields with given values.
+        By default comment creation and update dates are the same and comment like counter is 0.
+        
+        Args:
+            author_id (int or UUID): ID of the author of the comment as an integer or UUID object
+            text (str): comment text as a string"""
+
         self.author_id = author_id
         self.text = text
+
         self.create_data = datetime.now()
-        self.update_data = self.create_data
+        self.update_data = datetime.now()
         self.like_count = 0
 
-    def edit_comment(self, new_text):
+    def edit_comment(self, new_text: str) -> None:
+        """Updates comment text with provided value.
+        Sets comment last update date to the current date.
+        
+        Args:
+            new_text (str): new comment content as a string"""
+        
         self.text = new_text
+        
         self.update_data = datetime.now()
 
-    def like(self):
+    def like(self) -> None:
+        """Increments like counter"""
+
         self.like_count += 1
 
-    def dislike(self):
+    def dislike(self) -> None:
+        """Decrements like counter"""
+
         self.like_count -= 1
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """Service method for getting string representation of comment with all internal data"""
+
         return f"Comment(author_id={self.author_id}, text={self.text}, create_data={self.create_data}, update_data={self.update_data}, like_count={self.like_count})"
--- homework-1/project/utils/extract_prices.py
+++ homework-1/project/utils/extract_prices.py
@@ -1,2 +1,13 @@
-def extract_prices(products):
+from models.product import Product
+
+
+def extract_prices(products: list[Product]) -> list[float]:
+    """Extracts prices from list of products
+    
+    Args:
+        products (list[Product]): list of products as a list of Product objects
+    
+    Returns:
+        list[float]: list of prices as a list of real numbers"""
+
     return [product.get_price() for product in products]
