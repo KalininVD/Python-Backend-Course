cc7edcdf7bb03f3a09776f2188a2722bf4ba1588
Vlad_K <kalinin.vd@yandex.ru>
2024-12-29 15:12:06 +0300

    Added tests for MacOS version of Makefile

--- homework-4/project/test_controllers.py
+++ homework-4/project/test_controllers.py
@@ -4,6 +4,6 @@ from controllers import operation
 
 
 @pytest.mark.parametrize('a, b, expected', [(1, 2, 3), (5, -4, 1), (7, 8, 15), (-123, -456, -579), (12345, None, None), (None, -12345, None), (None, None, None)])
-def test_operation(a: int | None, b: int | None, expected: int | None):
+def test_operation(a: int | None, b: int | None, expected: int | None) -> None:
     received = operation(a, b)
     assert received == expected, f'Test for a={a}, b={b} failed: expected result is {expected}, received {received}'
