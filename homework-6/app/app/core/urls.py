6ca906ef165e3f8c5764ca5e80b4974fcb95670c
Vlad_K <kalinin.vd@yandex.ru>
2025-03-16 21:21:23 +0300

    Fixed Post and Comment like and unlike logic

--- homework-6/app/app/core/urls.py
+++ homework-6/app/app/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, PostViewSet, CommentViewSet
+from .views import UserViewSet, PostViewSet, CommentViewSet, LikePostView, LikeCommentView
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
@@ -9,4 +9,6 @@ router.register('comments', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('posts/<int:post_id>/like/', LikePostView.as_view(), name='like_post'),
+    path('comments/<int:comment_id>/like/', LikeCommentView.as_view(), name='like_comment'),
 ]
--- homework-6/app/app/core/views.py
+++ homework-6/app/app/core/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 from .models import User, Post, Comment
 from .serializers import UserSerializer, CreateUserSerializer, UpdateUserSerializer, PostSerializer, CommentSerializer
@@ -43,3 +46,51 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+
+class LikePostView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, post_id):
+        post = Post.objects.get(id=post_id)
+        user = request.user
+
+        if user in post.likes.all():
+            return Response({"detail": "You already liked this post."}, status=status.HTTP_400_BAD_REQUEST)
+
+        post.likes.add(user)
+        return Response({"detail": "Post liked successfully."}, status=status.HTTP_200_OK)
+
+    def delete(self, request, post_id):
+        post = Post.objects.get(id=post_id)
+        user = request.user
+
+        if user not in post.likes.all():
+            return Response({"detail": "You haven't liked this post."}, status=status.HTTP_400_BAD_REQUEST)
+
+        post.likes.remove(user)
+        return Response({"detail": "Like removed successfully."}, status=status.HTTP_200_OK)
+
+
+class LikeCommentView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, comment_id):
+        comment = Comment.objects.get(id=comment_id)
+        user = request.user
+
+        if user in comment.likes.all():
+            return Response({"detail": "You already liked this comment."}, status=status.HTTP_400_BAD_REQUEST)
+
+        comment.likes.add(user)
+        return Response({"detail": "Comment liked successfully."}, status=status.HTTP_200_OK)
+
+    def delete(self, request, comment_id):
+        comment = Comment.objects.get(id=comment_id)
+        user = request.user
+
+        if user not in comment.likes.all():
+            return Response({"detail": "You haven't liked this comment."}, status=status.HTTP_400_BAD_REQUEST)
+
+        comment.likes.remove(user)
+        return Response({"detail": "Like removed successfully."}, status=status.HTTP_200_OK)
