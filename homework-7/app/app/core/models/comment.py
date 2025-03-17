bcad4739f99fea7189055c8f70ec42fcee2af8e4
Vlad_K <kalinin.vd@yandex.ru>
2025-03-17 15:15:15 +0300

    Start HW7 from HW6 sources

---
+++ homework-7/app/app/core/models/comment.py
@@ -0,0 +1,15 @@
+from django.db import models
+from .user import User
+from .post import Post
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    likes = models.ManyToManyField(User, related_name="liked_comments", blank=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
